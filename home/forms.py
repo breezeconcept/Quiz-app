573487d3fe15c318aebbf0d79aa6080fdd131c0a
Breeze Concept <breezeconcept3@gmail.com>
2023-07-26 20:27:39 +0100

    updated

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,1 @@
 
-from django import forms
-
-class QuizForm(forms.Form):
-    def __init__(self, question, *args, **kwargs):
-        super(QuizForm, self).__init__(*args, **kwargs)
-        self.fields['answer'] = forms.ChoiceField(choices=[(answer.id, answer.answer_text) for answer in question.quizanswer_set.all()])
--- home/views.py
+++ home/views.py
@@ -1,21 +1,6 @@
-# quiz_app/views.py
-from django.shortcuts import render
-# from .models import QuizQuestion
-# from .forms import QuizForm
 
-# def quiz_view(request):
-#     if request.method == 'POST':
-#         form = QuizForm(request.POST)
-#         if form.is_valid():
-#             selected_answer_id = form.cleaned_data['answer']
-#             selected_answer = QuizAnswer.objects.get(pk=selected_answer_id)
-#             is_correct = selected_answer.is_correct
-#             # Implement your logic here to handle correct and incorrect answers
-#     else:
-#         question = QuizQuestion.objects.first()
-#         form = QuizForm(question)
-    
-#     return render(request, 'home/quiz.html', {'form': form})
+from django.shortcuts import render
+
 
 def quiz_view(request):
     return render(request, 'home/quiz.html')
