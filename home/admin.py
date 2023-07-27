e7c992d30e308d345544602a0dafb4dc2d5a3689
Breeze Concept <breezeconcept3@gmail.com>
2023-07-27 18:30:39 +0100

    updated

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.QuizQuestion)
-admin.site.register(models.QuizAnswer)
+admin.site.register(models.Quiz)
+
+class AnswerInline(admin.TabularInline):
+    model = models.Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
+admin.site.register(models.Question, QuestionAdmin)
+
+admin.site.register(models.Answer)
+
+admin.site.register(models.Result)
+
+
+
--- home/models.py
+++ home/models.py
@@ -1,17 +1,65 @@
 # quiz_app/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-class QuizQuestion(models.Model):
-    question_text = models.CharField(max_length=200)
+DIFF_CHOICES = (
+    ('easy', 'easy'),
+    ('medium', 'medium'),
+    ('hard', 'hard'),
+)
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="duration of the quiz in minutes")
+    required_score_to_pass = models.IntegerField(help_text="required score to pass")
+    difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return self.question_text
+        return f"{self.name}-{self.topic}"
+
+    def get_questions(self):
+        return self.question_set.all
 
 
-class QuizAnswer(models.Model):
-    question = models.ForeignKey(QuizQuestion, on_delete=models.CASCADE)
-    answer_text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
+    class Meta:
+        verbose_name_plural = 'Quizes'
+
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return self.answer_text
+        return str(self.text)
+
+    def get_answers(self):
+        return self.answer_set.all
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"question: {self.question.text}, answer: {self.text}, correct: {self.correct}"
+
+
+
+
+class Result(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.FloatField()
+
+    def __str__(self):
+        return str(self.pk)
